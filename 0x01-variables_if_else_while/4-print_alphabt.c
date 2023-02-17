e3e816a820f4ce4800c57c605999e627c158a05e
Alice Kaau <maingikaau@gmail.com>
2023-02-17 11:07:08 -0800

    Low level programming

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,10 +12,10 @@ int main(void)
 	{
 		if (c != 'e' && c != 'q')
 		{
-		putchar(c);
+			putchar(c);
 		}
-	c++;
+		c++;
 	}
-        putchar('\n');
-        return (0);
-}	
+	putchar('\n');
+	return (0);
+}
