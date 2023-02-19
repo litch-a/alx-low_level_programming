7521ca8c11aea2d1a057938718ad94affc0d04ae
Alice Kaau <maingikaau@gmail.com>
2023-02-19 04:16:28 -0800

    Update

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,10 +2,9 @@
 #include <stdio.h>
 #include <time.h>
 /**
- * mian - main block
- * Description: prints all single digit number of base 10
- * starting from 0 followed by new line
- * Return: 0
+ * main - program that prints all single digit numbers
+ * of base 10 starting from 0, followed by a new line.
+ * Return: 0 (Success)
  */
 int main(void)
 {
