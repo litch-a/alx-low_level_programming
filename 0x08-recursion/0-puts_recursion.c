3f971b320d12409a60bf61a8cd65471fff12a922
Alice Kaau <maingikaau@gmail.com>
2023-03-08 06:54:34 -0800

    Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints character by character recursively
+ *@s: string to print
+ *
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
+	else
+		_putchar('\n');
+}
