41c8318f2d195676936fab0e0ffccf66e28f2a7b
Alice Kaau <maingikaau@gmail.com>
2023-03-10 03:24:21 -0800

    C programming

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: number of argument
+ * @argv: array of arguments
+ *
+ * Return: 0 if runned with success
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: number of argument
+ * @argv: array of arguments
+ *
+ * Return: 0 if runned with success
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
